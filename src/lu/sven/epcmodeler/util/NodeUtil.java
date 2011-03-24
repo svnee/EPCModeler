1e7ad35b6703c6882c6daecb79f143809a495415
Phil <phil@ubuntu.(none)>
2011-03-24 14:14:47 +0100

    nodeutil added

---
+++ src/lu/sven/epcmodeler/util/NodeUtil.java
@@ -0,0 +1,27 @@
+package lu.sven.epcmodeler.util;
+
+import java.util.Collection;
+
+import edu.uci.ics.jung.graph.Graph;
+import lu.sven.epcmodeler.graph.Edge;
+import lu.sven.epcmodeler.graph.Node;
+
+public class NodeUtil {
+
+	public static Node getNodeById(String Id, Graph<Node,Edge> g){
+		
+		Node returnNode=null;
+		
+		Collection<Node> nodeList = g.getVertices();
+		
+		for(Node n:nodeList){
+			if((n.getID()).equals(Id)){
+				returnNode=n;
+				break;
+			}
+		}
+		
+		return returnNode;
+		
+	}
+}
